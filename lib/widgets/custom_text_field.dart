bdf9852fc3b40cda741c2e750508b1964a1aee81
Mahmoud Reda Shaaban <mrmrmr033@gmail.com>
2025-11-21 13:23:38 +0200

    start upload  Githup

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,23 +1,35 @@
 import 'package:first_project/app_colors.dart';
 import 'package:flutter/material.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   const CustomTextField({super.key, required  this.controller, required this.hint, required this.icon, this.secur=false});
   final  TextEditingController controller;
   final String hint;
   final IconData icon;
-  final IconData _subIcon =  Icons.remove_red_eye;
   final bool secur ;
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  final IconData _subIcon =  Icons.remove_red_eye;
+  late bool sec = false;
   @override
   Widget build(BuildContext context) {
+    sec = widget.secur;
     return TextField(
-      controller: controller ,
-      obscureText: secur,
+      controller: widget.controller ,
+      obscureText: sec,
       decoration: InputDecoration(
         filled: true,
-        hintText: hint,
-        prefixIcon: Icon(icon),
-        suffixIcon: secur ? Icon( _subIcon ): null,
+        hintText: widget.hint,
+        prefixIcon: Icon(widget.icon),
+        suffixIcon: sec ? IconButton( onPressed: (){
+          setState(() {
+            sec !=sec;
+          });
+        },icon: sec ?  Icon(_subIcon): Icon(Icons.remove_red_eye_outlined), ): null,
         fillColor: Color(0xffF2F2F2),
         border: OutlineInputBorder(),
         enabledBorder: OutlineInputBorder(
